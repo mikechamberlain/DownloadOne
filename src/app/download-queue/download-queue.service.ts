a270c275e04479d216449185834a9c4d61aecdc6
Mike Chamberlain <mikeachamberlain@gmail.com>
2017-04-05 23:30:30 +1000

    wip

--- src/app/download-queue/download-queue.service.ts
+++ src/app/download-queue/download-queue.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Download } from "../models/download";
 import { Observable, ReplaySubject } from "rxjs";
+declare const electron: any;
+const http = electron.remote.require('http');
+const fs = electron.remote.require('fs');
+
 
 @Injectable()
 export class DownloadQueueService {
@@ -14,6 +18,11 @@ export class DownloadQueueService {
     }
 
     enqueue(download: Download): void {
+        var req = http.get('http://speedtest.ftp.otenet.gr/files/test100k.db');
+        req.on('connect', function(res) {
+            res.pipe(fs.createWriteStream('~/Downloads/poo'));
+        });
+
         this.downloads.push(download);
         this.queueSubject.next(this.downloads);
     }
